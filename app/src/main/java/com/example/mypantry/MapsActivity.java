3161becc8cf0db55830fc34b398d9990a0f425c2
Behnam Ebrahimi <behnam.ebrahimi@gmail.com>
2019-09-01 12:16:07 +1000

    Nearby supermarkets added

--- app/src/main/java/com/example/mypantry/MapsActivity.java
+++ app/src/main/java/com/example/mypantry/MapsActivity.java
@@ -1,36 +1,20 @@
 package com.example.mypantry;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Build;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.fragment.app.FragmentActivity;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener,
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    LocationRequest request;
-    GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,60 +28,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        client = new GoogleApiClient.Builder(this).addApi(LocationServices.API)
-                .addConnectionCallbacks(this).addOnConnectionFailedListener(this)
-                .build();
+        // Add a marker in Sydney and move the camera
+        LatLng sydneyPosition = new LatLng(-33.813067, 151.0240488);
+        mMap.addMarker(new MarkerOptions().position(sydneyPosition).title("Sydney"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-33.818511,151.0190769))
+                .title("IGA").icon(BitmapDescriptorFactory.fromResource(R.mipmap.iga)));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-33.7955275,151.0450065))
+                .title("IGA").icon(BitmapDescriptorFactory.fromResource(R.mipmap.iga)));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-33.8086152,151.0066241))
+                .title("IGA").icon(BitmapDescriptorFactory.fromResource(R.mipmap.coles)));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-33.8244326,151.0790598))
+                .title("IGA").icon(BitmapDescriptorFactory.fromResource(R.mipmap.coles)));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-33.8146323,151.0544318))
+                .title("IGA").icon(BitmapDescriptorFactory.fromResource(R.mipmap.wool)));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-33.8126757,151.042099))
+                .title("IGA").icon(BitmapDescriptorFactory.fromResource(R.mipmap.aldi)));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydneyPosition, 12.0f));
 
-        client.connect();
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        if (location == null) {
-            Toast.makeText(getApplicationContext(), "Location not found!", Toast.LENGTH_SHORT).show();
-
-        } else {
-            LatLng latLngCurrent = new LatLng(location.getLatitude(), location.getLongitude());
-
-            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLngCurrent, 15);
-            mMap.animateCamera(update);
-
-            MarkerOptions options = new MarkerOptions();
-            options.position(latLngCurrent);
-            options.title("You are here");
-            mMap.addMarker(options);
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        request = new LocationRequest().create();
-        request.setInterval(1000);
-        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    Activity#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for Activity#requestPermissions for more details.
-            return;
-        }
-        LocationServices.FusedLocationApi.requestLocationUpdates(client, request, this);
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
